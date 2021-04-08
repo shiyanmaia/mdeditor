ba4f5b2bc75851a2621dc736a2a1e419aeaa3c85
heyichen <heyichen@tmxmall.com>
2021-04-08 14:16:55 +0800

    git init

---
+++ gulpfile.js
@@ -0,0 +1,48 @@
+var gulp = require('gulp'),
+    rename = require("gulp-rename"),
+    cleanCss = require("gulp-clean-css"),
+    uglify = require('gulp-uglify'),
+    jshint = require('gulp-jshint'),
+    ftp = require('gulp-ftp'),
+    syncConfig = require('./sync-config');
+
+
+gulp.task('jshint', function () {
+    return gulp.src('src/*.js')
+        .pipe(jshint())
+        .pipe(jshint.reporter('default'))
+        .pipe(uglify())
+        .pipe(rename({extname: ".min.js"}))
+        .pipe(gulp.dest('dist/'));
+});
+
+gulp.task('css', function () {
+    return gulp.src(['src/*.css'], {buffer: false})
+        .pipe(cleanCss())
+        .pipe(rename({extname: ".min.css"}))
+        .pipe(gulp.dest('dist/'));
+});
+
+
+gulp.task('sync', function () {
+    return gulp.src(['dist/*'])
+        .pipe(ftp(Object.assign({}, syncConfig, {remotePath: '/demo/mdeditor/dist'})));
+});
+
+gulp.task('sync-index', function () {
+    return gulp.src(['index.html'])
+        .pipe(ftp(Object.assign({}, syncConfig, {remotePath: '/demo/mdeditor/'})));
+});
+
+gulp.task('server-css', function () {
+    return gulp.src(['dist/*.css'])
+        .pipe(ftp(Object.assign({}, syncConfig, {remotePath: '/static/css/'})));
+});
+gulp.task('server-js', function () {
+    return gulp.src(['dist/*.js'])
+        .pipe(ftp(Object.assign({}, syncConfig, {remotePath: '/static/js/'})));
+});
+
+var tasks = ['jshint', 'css', 'sync', 'sync-index'];
+
+gulp.task('default', tasks);
